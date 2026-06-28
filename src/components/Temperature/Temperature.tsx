2afc082fbb3feded7abc45a5a08e8eca7eddb9c6
SohailShaikh2000 <sohailliyakatshaikh@gmail.com>
2026-06-28 14:23:15 +0530

    current weather data updated

--- src/components/Temperature/Temperature.tsx
+++ src/components/Temperature/Temperature.tsx
@@ -1,23 +1,66 @@
 import { useEffect, useState } from "react";
 import Background from "../../../assets/images/bg-today-large.svg";
-import TempLogo from "../../../assets/images/icon-drizzle.webp";
+import Drizzle from "../../../assets/images/icon-drizzle.webp";
+import Sunny from '../../../assets/images/icon-sunny.webp'
+import Storm from '../../../assets/images/icon-storm.webp'
+import Rain from '../../../assets/images/icon-rain.webp'
+import PartlyCloudy from '../../../assets/images/icon-partly-cloudy.webp'
+import Fog from '../../../assets/images/icon-fog.webp'
+import Snow from '../../../assets/images/icon-snow.webp'
+
 import { countries } from "country-data";
 
+interface weatherData {
+  base: string;
+  main: {
+    temp: number;
+  };
+  name: string;
+  sys: { country: string };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+}
+
 const Temperature = () => {
-  const [lat, setLat] = useState();
-  const [lon, setLon] = useState();
-  const [data, setData] = useState({});
+  const [lat, setLat] = useState<number | undefined>(undefined);
+  const [lon, setLon] = useState<number | undefined>(undefined);
+  const [data, setData] = useState<weatherData | null>(null);
 
   const API_key = import.meta.env.VITE_API_KEY;
 
-  const today = new Date()
-  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 'Dec']
+  const today = new Date();
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-  const dayName = dayNames[today.getDay()]
-  const monthName = monthNames[today.getMonth() + 1]
-  
-  console.log(today)
+  const dayName = dayNames[today.getDay()];
+  const monthName = monthNames[today.getMonth() + 1];
+
+  console.log(today);
 
   function successCallback(position: any) {
     const latitude = position.coords.latitude;
@@ -33,7 +76,7 @@ const Temperature = () => {
   console.log("lat", lat);
   console.log("lon", lon);
 
-  function errorCallback(error) {
+  function errorCallback(error: GeolocationPositionError) {
     switch (error.code) {
       case error.PERMISSION_DENIED:
         console.error("User denied the request for Geolocation.");
@@ -76,6 +119,28 @@ const Temperature = () => {
     }
   }, [lat, lon, API_key]);
 
+  const tempInCelcius = (tempInKelvin: number): number => {
+    return Math.round(tempInKelvin - 273.15);
+  };
+  if (!data) return <p>Loading...</p>;
+
+  const temp = tempInCelcius(data.main.temp);
+
+  const weatherImg = (weather: string) => {
+    switch(weather) {
+      case "Clouds":  return PartlyCloudy;
+      case "Sunny":  return Sunny;
+      case "Snow":  return Snow;
+      case "Rain":  return Rain;
+      case "Thunderstorm":  return Rain;
+      case "Drizzle":  return Drizzle;
+      case "Clear":  return Sunny;
+      case "Foggy": return Fog;
+      case "Storm": return Storm;
+      default: return Sunny;
+    }
+  }
+
   return (
     <div className="relative font-dm pb-6">
       <img src={Background} alt="" className="w-full" />
@@ -85,13 +150,17 @@ const Temperature = () => {
             {data.name}, <span>{countries[data.sys?.country]?.name}</span>
           </p>
           <p className="text-lg text-[hsl(250,6%,84%)]!">
-            {dayName}, <span>{today.getDate()} {monthName} {today.getFullYear()}</span>
+            {dayName},{" "}
+            <span>
+              {today.getDate()} {monthName} {today.getFullYear()}
+            </span>
           </p>
         </div>
         <div className="h-1/2 flex items-center justify-center gap-4">
-          <img src={TempLogo} alt="" className="h-23.75" />
+          <img src={weatherImg(data.weather[0].main)} alt="" className="h-23.75" />
           <h1 className="font-dm-italic text-8xl font-bold">
-            00<span>&deg;</span>
+            {temp}
+            <span>&deg;</span>
           </h1>
         </div>
       </div>
